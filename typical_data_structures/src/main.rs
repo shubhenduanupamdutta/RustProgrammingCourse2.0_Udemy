fa9b5d51e73b63f1e6fdf21cbd19a3f9532b9d37
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-08-17 21:22:07 +0530

    Typical Data Structures

---
+++ typical_data_structures/src/main.rs
@@ -0,0 +1,11 @@
+//-------------------------------------------------
+//           Typical Data Structures
+//-------------------------------------------------
+
+fn main() {
+    println!("############## Typical Data Structures in Rust. ##############\n");
+
+    println!("\n============ 1. Singly Linked List ============\n");
+    linked_list::main();
+
+}
