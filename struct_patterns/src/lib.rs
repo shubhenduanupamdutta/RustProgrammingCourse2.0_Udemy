03b0d90d9c37b1d28abef8a74ef80b4ce3d2cd83
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-08-18 20:01:26 +0530

    Useful Struct Patterns

---
+++ struct_patterns/src/lib.rs
@@ -0,0 +1,1 @@
+pub mod initializing_struct;
---
+++ struct_patterns/src/main.rs
@@ -0,0 +1,12 @@
+//----------------------------------------------
+//       Useful Struct Patterns
+//----------------------------------------------
+
+use struct_patterns::initializing_struct;
+
+fn main() {
+    println!("########### Useful Struct Patterns ###########");
+    println!();
+    println!("1. Initializing Struct Instance");
+    initializing_struct::main();
+}
